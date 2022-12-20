bb8a7f83e0e260ba80cecf4c4a3a0d47a0743bb9
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2022-12-20 00:53:40 -0500

    Update GameState.cs

--- GameState.cs
+++ GameState.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,19 +17,32 @@ namespace ICS4UFinalPerformanceTask
             private set { 
             currentBlock = value;
             currentBlock.Reset();
+                for (int i = 0; i < 2; i++)
+                {
+                    currentBlock.Move(1, 0);
+
+                    if(!BlockFits())
+                    {
+                        currentBlock.Move(-1, 0);
+                    }
+                }
             }
         }
 
         public GameGrid GameGrid { get; }
         public BlockQueue BlockQueue { get; }
         public bool GameOver { get; private set; }
+        public int Score { get; private set; }
+        public Block HeldBlock { get; private set; }
+        public  bool CanHold { get; private set; }
+
 
         public GameState() {
 
             GameGrid = new GameGrid(22, 10);
             BlockQueue = new BlockQueue();
             CurrentBlock = BlockQueue.GetAndUpdate();
-
+            CanHold = true;
         }
 
         private bool BlockFits()
@@ -46,6 +59,29 @@ namespace ICS4UFinalPerformanceTask
 
         }
 
+        public void HoldBlock()
+        {
+            if (!CanHold)
+            {
+                return;
+            }
+
+            if (HeldBlock == null)
+            {
+                HeldBlock = CurrentBlock;
+                CurrentBlock = BlockQueue.GetAndUpdate();
+            }
+
+            else {
+
+                Block tmp = CurrentBlock;
+                CurrentBlock = HeldBlock;
+                HeldBlock = tmp;
+
+            }
+
+            CanHold = false;
+        }
         public void RotateBlockCW() {
 
             CurrentBlock.RotateCW();
@@ -110,7 +146,7 @@ namespace ICS4UFinalPerformanceTask
 
             }
 
-            GameGrid.ClearFullRows();
+            Score += GameGrid.ClearFullRows();
 
             if (IsGameOver())
             {
@@ -120,6 +156,7 @@ namespace ICS4UFinalPerformanceTask
             }
             else {
                 CurrentBlock = BlockQueue.GetAndUpdate();
+                CanHold = true;
             }
         }
 
@@ -135,6 +172,35 @@ namespace ICS4UFinalPerformanceTask
             }
 
         }
+        private int TileDropDistance(Position p)
+        {
+            int drop = 0;
 
+            while  (GameGrid.IsEmpty(p.Row + drop+1, p.Column))
+            {
+                drop++;
+            }
+
+            return drop;
+        }
+
+        public int BlockDropDistance()
+        {
+            int drop = GameGrid.Rows;
+
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                drop = System.Math.Min(drop, TileDropDistance(p));
+            }
+
+            return drop;
+
+        }
+
+        public void DropBlock()
+        {
+            CurrentBlock.Move(BlockDropDistance(), 0);
+            PlaceBlock();
+        }
     }
 }
